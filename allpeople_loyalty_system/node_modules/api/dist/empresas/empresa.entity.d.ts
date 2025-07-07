551c2c2ce26ef2431bc99f3fb2aa012e673d487b
JulianPenuela13 <julianandresp2010@gmail.com>
2025-07-07 11:13:13 -0500

    chore: aplicar .gitignore y limpiar archivos ignorados

--- allpeople_loyalty_system/node_modules/api/dist/empresas/empresa.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../users/user.entity';
-export declare class Empresa {
-    id: number;
-    nombre_empresa: string;
-    plan_suscripcion: string;
-    fecha_creacion: Date;
-    users: User[];
-}
